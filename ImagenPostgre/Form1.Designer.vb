a6c7bf779aa75d795d991269c985b6bff069c921
Antonio Rodríguez <antonio@iessanvicente.com>
2015-11-13 00:30:28 +0100

    Versión Inicial

---
+++ ImagenPostgre/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(275, 285)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(189, 303)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 30)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Insertar imagen"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 313)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(171, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(293, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(275, 285)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(293, 313)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(171, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(470, 303)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(98, 30)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Cargar imagen"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(580, 363)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ ImagenPostgre/Form1.vb
@@ -0,0 +1,122 @@
+﻿Option Explicit On
+Option Strict On
+
+Imports System.IO
+Imports Npgsql
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            'Suponiendo que tengas la imagen en un archivo, necesitaras un objeto fileStream para leer la imagen 
+            'y otro FileInfo para obtener el tamaño. Además necesitarás un array de bytes para convertir la imagen 
+            'antes de pasarsela a la base de datos.
+
+            Dim imgInfo As New FileInfo(OpenFileDialog1.FileName)
+            Dim imgStream As FileStream
+            Dim imgByte(Convert.ToInt32(imgInfo.Length)) As Byte
+
+            imgStream = imgInfo.OpenRead()
+
+            ' Ahora cargamos la imagen en el array de bytes
+            imgStream.Read(imgByte, 0, Convert.ToInt32(imgInfo.Length))
+            imgStream.Close()
+
+            ' Conectamos con la base de datos
+            Dim BD As New BaseDeDatos()
+            BD.InsertarImagen(TextBox1.Text, imgByte)
+
+            'Cargamos la imagen en el PictureBox
+            PictureBox1.Load(OpenFileDialog1.FileName)
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim BD As New BaseDeDatos()
+
+        'Cargamos la imagen en el PictureBox desde la base de datos. Se busca por nombre del pais que hay en el textbox.
+        PictureBox2.Image = BD.LeerImagen(TextBox2.Text)
+
+    End Sub
+End Class
+
+Public Class BaseDeDatos
+    Private ConexionConBD As NpgsqlConnection
+    Private Orden As NpgsqlCommand
+    Private Lector As NpgsqlDataReader
+
+    Public Sub InsertarImagen(ByVal NombrePais As String, ByVal img As Byte())
+
+        'Abrir la base de datos
+        Dim strConexión As String = String.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",
+                                                  "192.168.0.2", "5432", "esports", "antonio", "esports")
+        ConexionConBD = New NpgsqlConnection(strConexión)
+        ConexionConBD.Open()
+
+        'Creamos una consulta parametrizada. Usamos @nombre para los parámetros
+        Dim Consulta As String = "INSERT INTO pais(nombre_pais, bandera_pais) VALUES (@pais, @imagen)"
+
+        'Creamos la consulta y le asignamos los parámetros.
+        Orden = New NpgsqlCommand(Consulta, ConexionConBD)
+        Orden.Parameters.Add(New Npgsql.NpgsqlParameter("@imagen", NpgsqlTypes.NpgsqlDbType.Bytea))
+        Orden.Parameters("@imagen").Value = img
+        Orden.Parameters.Add(New Npgsql.NpgsqlParameter("@pais", NpgsqlTypes.NpgsqlDbType.Varchar))
+        Orden.Parameters("@pais").Value = NombrePais
+
+        Orden.ExecuteNonQuery()
+        CerrarConexion()
+
+    End Sub
+
+    Public Function LeerImagen(ByVal NombrePais As String) As Image
+        Dim tam As Integer = 0
+        Dim img() As Byte = {}
+
+        'Abrir la base de datos
+        Dim strConexión As String = String.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",
+                                                  "192.168.0.2", "5432", "esports", "antonio", "esports")
+        ConexionConBD = New NpgsqlConnection(strConexión)
+        ConexionConBD.Open()
+
+        'Creamos una consulta parametrizada. Usamos @nombre para los parámetros
+        Dim Consulta As String = "Select bandera_pais, length(bandera_pais) as len from pais where nombre_pais = @pais"
+
+        'Creamos la consulta y le asignamos los parámetros.
+        Orden = New NpgsqlCommand(Consulta, ConexionConBD)
+        Orden.Parameters.Add(New Npgsql.NpgsqlParameter("@pais", NpgsqlTypes.NpgsqlDbType.Varchar))
+        Orden.Parameters("@pais").Value = NombrePais
+
+        Lector = Orden.ExecuteReader()
+
+        'Recogemos los datos obtenidos por la select, la imagen y su longitud.
+        While Lector.Read()
+            tam = CInt(Lector("len"))
+            ReDim img(tam)
+            img = CType(Lector("bandera_pais"), Byte())
+        End While
+
+        CerrarConexion()
+
+        'Finalmente necesitaremos un MemoryStream para convertir la imagen a un objeto image de .Net
+        Dim ms As New MemoryStream
+        Dim imagen As Image
+
+        ms.Write(img, 0, tam)
+        imagen = Image.FromStream(ms)
+
+        Return imagen
+    End Function
+
+    Public Sub CerrarConexion()
+        ' Cerrar la conexión cuando ya no sea necesaria
+        If (Not Lector Is Nothing) Then
+            Lector.Close()
+        End If
+        If (Not ConexionConBD Is Nothing) Then
+            ConexionConBD.Close()
+        End If
+    End Sub
+
+End Class
